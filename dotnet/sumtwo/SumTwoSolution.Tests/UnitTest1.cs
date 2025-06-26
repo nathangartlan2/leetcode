df54d01584ba6ec1dc7f5b4aa0bc8f5981398706
nathangartlan2 <nathangartlan@gmail.com>
2025-06-26 11:03:23 +0100

    solved in dotnet

---
+++ dotnet/sumtwo/SumTwoSolution.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿namespace SumTwoSolution.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        SumTwoSolution s = new SumTwoSolution();
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        int[] output = new int[] { 0, 1 };
+        Assert.Equal(output, s.TwoSum(nums, target));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        SumTwoSolution s = new SumTwoSolution();
+        int[] nums = new int[] { 3, 2, 4 };
+        int target = 6;
+        int[] output = new int[] { 1, 2 };
+        Assert.Equal(output, s.TwoSum(nums, target));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        SumTwoSolution s = new SumTwoSolution();
+        int[] nums = new int[] { 3, 3 };
+        int target = 6;
+        int[] output = new int[] { 0, 1 };
+        Assert.Equal(output, s.TwoSum(nums, target));
+    }
+
+}
---
+++ dotnet/sumtwo/SumTwoSolution/SumTwoSolution.cs
@@ -0,0 +1,35 @@
+﻿namespace SumTwoSolution;
+
+public class SumTwoSolution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        Dictionary<int, int> seen = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int currentNumber = nums[i];
+
+            int differnece = target - currentNumber;
+
+            if (seen.ContainsKey(differnece))
+            {
+                int firstLocation = seen[differnece];
+                int secondLocation = i;
+                return new int[] { firstLocation, secondLocation };
+            }
+            else
+            {
+                if (!seen.ContainsKey(currentNumber))
+                {
+                    seen.Add(currentNumber, i);
+                }
+            }
+
+        }
+
+        return new int[] { };
+
+    }
+}
